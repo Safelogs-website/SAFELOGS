b6a3afd80c1b68324e2ec964819cb2b3c8969229
Safelogs-website <buyaccts039@gmail.com>
2026-08-29 19:02:12 +0100

    Add 2020 to years array in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,6 +16,7 @@ import {
 import SelectField from './SelectField';
 
 const years = [
+  { value: '2020', label: '2020' },
   { value: '2021', label: '2021' },
   { value: '2022', label: '2022' },
   { value: '2023', label: '2023' },
